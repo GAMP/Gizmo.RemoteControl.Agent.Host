3f88d8eee7ef914380eb8fdd2e8cb91220806e01
Andrey Pestunov <masterlifting@gmail.com>
2024-03-14 07:10:14 +0100

    init

---
+++ Gizmo.RemoteControl.Agent.Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using Gizmo.RemoteControl.Agent.Windows;
+
+using Microsoft.Extensions.DependencyInjection;
+
+var host = "http://localhost:81";
+var clientId = "695468c9-93f7-47ea-8622-85495b2e04f9";
+var accessKey = "password";
+
+if (args.Length > 0)
+{
+    try
+    {
+        host = args[0];
+        clientId = args[1];
+        accessKey = args[2];
+    }
+    catch
+    {
+        Console.WriteLine("Invalid arguments to start the agent.");
+        return;
+    }
+}
+
+var services = new ServiceCollection();
+services.AddRemoteControlServices();
+var provider = services.BuildServiceProvider();
+var result = await provider.UseHeadlessClient(host, clientId, accessKey);
+
+Console.WriteLine(result);
